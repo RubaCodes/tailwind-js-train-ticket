7036386b09f3508001ccdbf4e657f01c5d9aba7d
rubacodes <rubacodes@gmail.com>
2022-04-30 12:28:07 +0200

    acquisizione dei dati completata

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('test');
+let userName = document.getElementById('user-name').value;
+let distance = document.getElementById('user-distance').value;
+let userAge = document.getElementById('user-age').value;
+
+
+console.log(userName);
+console.log(distance);
+console.log(userAge);
