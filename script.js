63ce03d93d3df2b304c0bb53c080b6c1ffbd86af
rubacodes <rubacodes@gmail.com>
2022-04-30 14:06:31 +0200

    implementato il refresh

--- script.js
+++ script.js
@@ -1,30 +1,21 @@
-//hook dei lavori dei form
-let userName = document.getElementById('user-name-input').value;
-let distance = Number(document.getElementById('user-distance-input').value);
-let userAge = document.getElementById('user-age-input').value;
-
-console.log(userName);
-console.log(distance);
-console.log(userAge);
-
-//event listenerd on clicks
 //genera biglietto
 const generateButton = document.getElementById('generate');
 generateButton.addEventListener('click', 
     function(){
-        alert('generato');
+        let userNameInput = document.getElementById('user-name-input').value;
+        let userDistanceInput = Number(document.getElementById('user-distance-input').value);
+        let userAgeInput = document.getElementById('user-age-input').value;
+        document.getElementById('user-name').innerHTML = userNameInput;
+        document.getElementById('user-distance').innerHTML = userDistanceInput;
+        document.getElementById('user-age').innerHTML = userAgeInput;
     }
 )
 //refresh biglietto
 const refreshButton = document.getElementById('refresh');
 refreshButton.addEventListener('click', 
     function(){
-        userName = "";
-        distance = 0;
-        userAge = "";
-        
-        console.log(userName);
-        console.log(distance);
-        console.log(userAge);
+        document.getElementById('user-name').innerHTML = '';
+        document.getElementById('user-distance').innerHTML = '';
+        document.getElementById('user-age').innerHTML = '';
     }
 )
