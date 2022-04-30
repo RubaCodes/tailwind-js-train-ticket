c8cf267aa10b71609b6219b96c2c3f069617a8f2
rubacodes <rubacodes@gmail.com>
2022-04-30 14:57:22 +0200

    implementata la logica sul prezzo

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
+//hook ai valori di input
+let userNameInput = document.getElementById('user-name-input').value;
+let userDistanceInput = Number(document.getElementById('user-distance-input').value);
+let userAgeInput = document.getElementById('user-age-input').value;
+
+//price per unit
+const pricePerKm = 0.21;
+
+
+//parametri sconto
+const overAge = 65;
+const underAge = 18;
+const overAgeDiscount = 0.6;
+const underAgeDiscount = 0.8;
+
+
 //genera biglietto
 const generateButton = document.getElementById('generate');
 generateButton.addEventListener('click', 
     function(){
-        let userNameInput = document.getElementById('user-name-input').value;
-        let userDistanceInput = Number(document.getElementById('user-distance-input').value);
-        let userAgeInput = document.getElementById('user-age-input').value;
+        // inserimento valori statici
         document.getElementById('user-name').innerHTML = userNameInput;
         document.getElementById('user-distance').innerHTML = userDistanceInput;
         document.getElementById('user-age').innerHTML = userAgeInput;
+        //calcolo prezzo
+        let price;
+        if (userAgeInput >= overAge) {
+            price = userDistanceInput * pricePerKm * overAgeDiscount;
+        } else if (userAgeInput < underAge) {
+            price = userDistanceInput * pricePerKm * underAgeDiscount;
+        } else {
+            price = userDistanceInput * pricePerKm;
+        }
+        //set del prezzo sul document
+        document.getElementById('final-price').innerHTML = price.toFixed(2) + " $"
+        //apparizione ticket
         const ticket = document.getElementById('ticket');
         ticket.className = 'flex my-20 rounded-lg overflow-hidden';
     }
