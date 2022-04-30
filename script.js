5773c7d1a04cd741f723174d64d2833a89b4d13c
rubacodes <rubacodes@gmail.com>
2022-04-30 15:21:13 +0200

    refactoring e bugfix

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-//hook ai valori di input
-let userNameInput = document.getElementById('user-name-input').value;
-let userDistanceInput = Number(document.getElementById('user-distance-input').value);
-let userAgeInput = document.getElementById('user-age-input').value;
-
 //price per unit
 const pricePerKm = 0.21;
 
 
 //parametri sconto
-const overAge = 65;
-const underAge = 18;
+const overAge = 'elder';
+const underAge = 'minorenne';
 const overAgeDiscount = 0.6;
 const underAgeDiscount = 0.8;
 
@@ -18,15 +13,18 @@ const underAgeDiscount = 0.8;
 const generateButton = document.getElementById('generate');
 generateButton.addEventListener('click', 
     function(){
+        const userNameInput = document.getElementById('user-name-input').value;
+        const userDistanceInput = Number(document.getElementById('user-distance-input').value);
+        const userAgeInput = document.getElementById('user-age-input').value;
         // inserimento valori statici
         document.getElementById('user-name').innerHTML = userNameInput;
         document.getElementById('user-distance').innerHTML = userDistanceInput;
         document.getElementById('user-age').innerHTML = userAgeInput;
         //calcolo prezzo
         let price;
-        if (userAgeInput >= overAge) {
+        if (userAgeInput === overAge) {
             price = userDistanceInput * pricePerKm * overAgeDiscount;
-        } else if (userAgeInput < underAge) {
+        } else if (userAgeInput === underAge) {
             price = userDistanceInput * pricePerKm * underAgeDiscount;
         } else {
             price = userDistanceInput * pricePerKm;
@@ -42,9 +40,9 @@ generateButton.addEventListener('click',
 const refreshButton = document.getElementById('refresh');
 refreshButton.addEventListener('click', 
     function(){
-        document.getElementById('user-name').innerHTML = '';
-        document.getElementById('user-distance').innerHTML = '';
-        document.getElementById('user-age').innerHTML = '';
+        // document.getElementById('user-name').innerHTML = '';
+        // document.getElementById('user-distance').innerHTML = '';
+        // document.getElementById('user-age').innerHTML = '';
         const ticket = document.getElementById('ticket');
         ticket.className = 'hidden';
     }
