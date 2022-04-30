a1087180de6cceccf9a40673cd3aae2f4894b708
rubacodes <rubacodes@gmail.com>
2022-04-30 12:33:12 +0200

    aggiuniti listender ai button

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
+//hook dei lavori dei form
 let userName = document.getElementById('user-name').value;
 let distance = Number(document.getElementById('user-distance').value);
 let userAge = document.getElementById('user-age').value;
 
-
 console.log(userName);
 console.log(distance);
 console.log(userAge);
+
+//event listenerd on clicks
+//genera biglietto
+const generateButton = document.getElementById('generate');
+generateButton.addEventListener('click', 
+    function(){
+        alert('generato');
+    }
+)
+//refresh biglietto
+const refreshButton = document.getElementById('refresh');
+refreshButton.addEventListener('click', 
+    function(){
+        alert('refreshato');
+    }
+)
